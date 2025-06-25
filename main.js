ac8a5b6555b995abfcd81aa0992e853f3e13a331
Teguh Dwi Prasetyo <teguhdwi460@gmail.com>
2025-06-25 20:49:58 +0700

    Remove flip-card functionality and associated styles from main.js and style.css

--- main.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.flip-card').forEach(function(card) {
-    card.addEventListener('click', function() {
-        this.querySelector('.flip-card-inner').classList.toggle('flipped');
-    });
-});
